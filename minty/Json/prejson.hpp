d380688a39b9f28c3d19a0212d9f100a695c466f
mintygingy <bnctfbdb@gmail.com>
2023-04-22 20:54:51 +0300

    some launcher work

--- minty/Json/prejson.hpp
+++ minty/Json/prejson.hpp
@@ -8,6 +8,18 @@ std::filesystem::path settings_path = std::filesystem::current_path() / "minty";
 std::ofstream settings_fileO(settings_path);
 std::ifstream settings_fileI(settings_path);
 
+bool readConfigToObject(nlohmann::json jsonObject) {
+	if (settings_fileI.is_open()) {
+		settings_fileI >> jsonObject;
+		settings_fileI.close();
+		return true;
+	}
+	else {
+		std::cout << "Failed to read config." << std::endl;
+		return false;
+	}
+}
+
 bool writeToJsonConfig(std::string filename, nlohmann::json jsonObject) {
 	// Write the updated JSON object to file
 	std::ifstream config_file(filename);
