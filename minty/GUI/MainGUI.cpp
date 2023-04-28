668ec1437487257b8de53cd07f790872a97c34a1
mintygingy <bnctfbdb@gmail.com>
2023-04-28 19:35:57 +0300

    imgui typos fix

--- minty/GUI/MainGUI.cpp
+++ minty/GUI/MainGUI.cpp
@@ -10,56 +10,58 @@ namespace gui {
 
         setlocale(LC_ALL, "C");
 
-		ImGui::Begin("Minty");
-
-            if (ImGui::BeginTabItem("Main"))
+        ImGui::Begin("Minty");
+        ImGui::BeginTabBar("Minty");
+        if (ImGui::BeginTabItem("Main"))
+        {
+            /*if (!is_hook_success)
             {
-                /*if (!is_hook_success)
-                {
-                    ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "WARNING! LUA NOT HOOKED");
-                    ImGui::Separator();
-                }*/
-                if (ImGui::Button("PLEASE WORK")) {
-                    il2fns::MoleMole__ActorUtils__ShowMessage_I();
-                }
-		ImGui::BeginGroup();
-        static bool block_key = false;
-
-		if (ImGui::Checkbox("Block key/mouse", &block_key)) {
-			//renderer::SetInputLock(this, m_IsBlockingInput);
-		}
-
-        static int SelectedSection = 0;
-
-        if (ImGui::ListBoxHeader("##CategorySelect", ImVec2(175, -FLT_MIN))) {
-            for (int i = 0; i < ModuleOrder.size(); i++) {
-                bool is_selected = (i == SelectedSection);
-                if (ImGui::Selectable(ModuleOrder[i].c_str(), is_selected)) {
-                    SelectedSection = i;
-                }
+                ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "WARNING! LUA NOT HOOKED");
+                ImGui::Separator();
+            }*/
+            if (ImGui::Button("PLEASE WORK")) {
+                il2fns::MoleMole__ActorUtils__ShowMessage_I();
             }
-            ImGui::ListBoxFooter();
-            ImGui::SetItemDefaultFocus();
+            ImGui::BeginGroup();
+            static bool block_key = false;
+
+            if (ImGui::Checkbox("Block key/mouse", &block_key)) {
+                //renderer::SetInputLock(this, m_IsBlockingInput);
+            }
+
+            static int SelectedSection = 0;
+
+            if (ImGui::ListBoxHeader("##CategorySelect", ImVec2(175, -FLT_MIN))) {
+                for (int i = 0; i < ModuleOrder.size(); i++) {
+                    bool is_selected = (i == SelectedSection);
+                    if (ImGui::Selectable(ModuleOrder[i].c_str(), is_selected)) {
+                        SelectedSection = i;
+                    }
+                }
+                ImGui::ListBoxFooter();
+                ImGui::SetItemDefaultFocus();
+            }
+
+            ImGui::EndGroup();
+
+            ImGui::SameLine();
+
+            ImGui::BeginGroup();
+
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_None;
+            ImGui::PushStyleVar(ImGuiStyleVar_ChildRounding, 5.0f);
+            ImGui::BeginChild("ChildR", ImVec2(0, 0), true, window_flags);
+
+            DrawSection(ModuleOrder[SelectedSection]);
+
+            ImGui::EndChild();
+            ImGui::PopStyleVar();
+            ImGui::EndTabItem();
+            ImGui::EndGroup();
+            ImGui::EndTabBar();
+
+            ImGui::End();
+
         }
-
-		ImGui::EndGroup();
-
-		ImGui::SameLine();
-
-		ImGui::BeginGroup();
-
-		ImGuiWindowFlags window_flags = ImGuiWindowFlags_None;
-		ImGui::PushStyleVar(ImGuiStyleVar_ChildRounding, 5.0f);
-		ImGui::BeginChild("ChildR", ImVec2(0, 0), true, window_flags);
-
-        DrawSection(ModuleOrder[SelectedSection]);
-
-		ImGui::EndChild();
-		ImGui::PopStyleVar();
-
-		ImGui::EndGroup();
-
-		ImGui::End();
-        
     }
 }
