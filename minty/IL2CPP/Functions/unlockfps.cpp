1b7b820309b98e811d832bbcebce7402301f6e0e
mintygingy <bnctfbdb@gmail.com>
2023-07-09 16:23:03 +0300

    unlock fps fix im dumb

--- minty/IL2CPP/Functions/unlockfps.cpp
+++ minty/IL2CPP/Functions/unlockfps.cpp
@@ -7,13 +7,14 @@ namespace il2fns {
 		static int target = 60;
 		if (ImGui::Checkbox("Unlock FPS", &ifunlock))
 			if (!ifunlock)
-				app::UnityEngine__Application__set__targetFramerate(60);
+				app::UnityEngine__Application__set__targetFramerate(60); app::UnityEngine__QualitySettings__set__vSyncCount(1);
 		ImGui::SameLine();
 		HelpMarker("Unlocks framerate to target value.");
 		if (ifunlock) {
+			app::UnityEngine__QualitySettings__set__vSyncCount(0);
 			ImGui::Indent();
 			if (ImGui::SliderInt("Target FPS", &target, 10, 360))
-				app::UnityEngine__Application__set__targetFramerate(target);
+				app::UnityEngine__Application__set__targetFramerate(target); 
 			ImGui::Unindent();
 		}
 		ImGui::Checkbox("Energy-saving mode", &ifsave);
