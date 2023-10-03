9a60997341d25df77f7bc2aaec0e1e3e0a167240
wat3red <roma3koil@gmail.com>
2023-10-03 22:44:38 +0200

    pryanya fix lnk2001 plz

---
+++ minty/Functions/Settings/Settings.cpp
@@ -0,0 +1,86 @@
+#include "Settings.h"
+
+namespace cheat {
+	void Settings::GUI() {
+        initDelay = getSmthFromJson(0, "general", "initDelay");
+        show_rpc = getSmthFromJson(0, "general", "showRPC");
+
+        ImGui::SeparatorText("General");
+
+        ImGui::Checkbox("Show ImGui's cursor", &ImGui::GetIO().MouseDrawCursor);
+
+        if (ImGui::Checkbox("Show current FPS", &ifShowFPS)) {
+            saveSmthToJson(ifShowFPS, "general", "ShowFPS");
+        }
+
+        //ImGui::Checkbox("Block input", &block_input);
+
+        if (ImGui::Checkbox("Show Discord RPC", &show_rpc)) {
+            saveSmthToJson(show_rpc, "general", "showRPC");
+        }
+        ImGui::SameLine(); 
+        HelpMarker("Turn Discord custom RPC on/off. Requires re-entering game.");
+
+        if (ImGui::SliderInt("Initialization delay (ms)", &initDelay, 0, 60000)) {
+            saveSmthToJson(initDelay, "general", "initDelay");
+        }
+        ImGui::SameLine(); 
+        HelpMarker("Change delay before showing menu. May cause lags while opening, so try to change this value in case.");
+
+        ImGui::SeparatorText("Theme");
+        /*----------PLACEHOLDER----------*/
+        static int themeIndex = 1;
+
+        if (ImGui::RadioButton("Dark", &themeIndex, 1)) {
+            settheme(1);
+            saveSmthToJson(1, "theme", "color");
+
+        }
+
+        if (ImGui::RadioButton("Light", &themeIndex, 2)) {
+            settheme(2);
+            saveSmthToJson(2, "theme", "color");
+
+        }
+
+        /*if (ImGui::RadioButton("Red", &themeIndex, 3)) {
+            settheme(3);
+            cfgjsonobj["theme"]["color"] = 3;
+            config_json.merge_patch(cfgjsonobj);
+            std::ofstream merged_file("minty");
+            merged_file << config_json.dump(4);
+            merged_file.close();
+        }*/
+
+        ImGui::SeparatorText("Style");
+
+        static int themestyleindex = 1;
+
+        if (ImGui::RadioButton("Cozy", &themestyleindex, 1)) {
+            setstyle(1);
+            saveSmthToJson(1, "theme", "style");
+
+        }
+
+        if (ImGui::RadioButton("Cozy Squared", &themestyleindex, 2)) {
+            setstyle(2);
+            saveSmthToJson(2, "theme", "style");
+
+        }
+
+        ImGui::SeparatorText("Font");
+
+        static int fontSelectionIndex = 0;
+        if (ImGui::RadioButton("Normal", &fontSelectionIndex, 0)) {
+            setfont(1);
+        }
+
+        if (ImGui::RadioButton("Bold", &fontSelectionIndex, 1)) {
+            setfont(2);
+        }
+
+        ImGui::SeparatorText("Customize");
+        //ImGui::TextDisabled("DEBUG");
+        ImGui::Checkbox("Show Style Editor", &show_style_editor);
+	}
+}
